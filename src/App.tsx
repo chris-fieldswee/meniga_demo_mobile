bd5549131d77aa19cbe0cb3a47f46bfb5ac14626
chris-fieldswee <133598020+chris-fieldswee@users.noreply.github.com>
2025-08-26 19:40:35 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,18 @@
-
+import React from 'react';
+import { DashboardScreen } from './components/dashboard/DashboardScreen';
+import { BottomNavigation } from './components/dashboard/BottomNavigation';
+import { Header } from './components/dashboard/Header';
+export function App() {
+  return <div className="flex w-full min-h-screen justify-center items-center bg-gray-100 p-4">
+      <div className="max-w-[375px] mx-auto">
+        <div className="rounded-[60px] overflow-hidden shadow-2xl border-8 border-black relative bg-white">
+          <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-1/3 h-7 bg-black rounded-b-xl z-10"></div>
+          <div className="flex flex-col h-[750px]">
+            <Header />
+            <DashboardScreen />
+            <BottomNavigation />
+          </div>
+        </div>
+      </div>
+    </div>;
+}
